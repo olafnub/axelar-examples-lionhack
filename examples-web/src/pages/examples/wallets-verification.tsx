79055376669928277db10ac2ca24d254eefae5e1
olafnub <googlhoogle@gmail.com>
2023-04-16 02:49:12 -0400

    Create template of full body card with card 2

--- examples-web/src/pages/examples/wallets-verification.tsx
+++ examples-web/src/pages/examples/wallets-verification.tsx
@@ -5,6 +5,9 @@ import { isTestnet } from "config/constants";
 function clickMe() {
   alert('hi');
 }
+// function handleChanges(event: HTMLInputElement) {
+//   // console.log(event.value);
+// }
 
 class Metamask extends Component {
     constructor(props: any) {
@@ -19,17 +22,51 @@ class Metamask extends Component {
           </div>
 
           {/* {Full Body Card} */}
-          <div className="grid grid-cols-2 gap-20 mt-20 justify-items-center">
+          <div className="grid grid-cols-3 grid-rows-2 gap-20 mt-20 justify-items-center">
             {/* {Left Card - Source chain} */}
-            <div className="row-span-2 shadow-xl card w-96 bg-base-100">
-              <form>
-                <label>Chain 1</label>
+            <div className="row-span-5 shadow-xl card w-96 bg-base-100">
+              <form className="flex form-control col-span-3">
+                <label htmlFor="chain-1" className="w-full input-group">Chain 1:
+                </label>
+                <input 
+                  type="text"
+                  placeholder="Enter address"
+                  className="w-full input input-bordered"
+                  id="chain-1"
+                  ></input>
 
+                <input
+                  type="text"
+                  placeholder="Enter message"
+                  className="w-full input input-bordered"
+                
+                ></input>
               </form>
+            </div>
+
+            {/* Right Card - Dest chain */}
+            <div className="row-span-5 shadow-xl card w-96 bg-base-100">
+              <form className="flex form-control">
+                <label htmlFor="chain-2" className="w-full input-group">Chain 2:
+                </label>
+                <input 
+                  type="text"
+                  placeholder="Enter address"
+                  className="w-full input input-bordered"
+                  ></input>
+
+                <input
+                  type="text"
+                  placeholder="Enter message"
+                  className="w-full input input-bordered"
+                
+                ></input>
+              </form>
+            </div>
+            
               <div className="submit-run">
                 <button className="btn btn-primary" onClick={clickMe}>RUN</button>
               </div>
-            </div>
           </div>
           </>
         )
