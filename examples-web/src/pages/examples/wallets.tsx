f2a098490ad6095fff5e3104009888138c9304ce
olafnub <googlhoogle@gmail.com>
2023-04-16 00:12:13 -0400

    first lionhack

---
+++ examples-web/src/pages/examples/wallets.tsx
@@ -0,0 +1,17 @@
+import React, { Component, useState} from 'react';
+import ethers, { } from 'ethers';
+import { isTestnet } from "config/constants";
+
+class Metamask extends Component {
+    constructor(props: any) {
+      super(props);
+    }
+
+    render() {
+        return(
+          <div>
+          </div>
+        )
+      }
+    }
+
